e6b3db69e6526982ee41449e2fe5d1d474612712
Patooh <53504041+Patooh777@users.noreply.github.com>
2024-05-07 16:27:51 -0300

    Update QuestSystem.lua

--- Cero.lua
+++ Cero.lua
@@ -1,181 +1,62 @@
-	local TW = game:GetService("TweenService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local QuestItems = workspace.Map.QuestItems
+local QuestRmt = game:GetService("ReplicatedStorage").Assets.Remotes.QuestRemote
 
-local Assets = ReplicatedStorage.Assets
-local Utilities = require(Assets.Modules.Utils)
-local CameraShake = require(Assets.Modules.CameraShakeHandler)
+local QuestSystem = {}
+QuestSystem.Reward = 100
+QuestSystem.Needed = 3
 
-local Debounce = false
-local Holding = false
+QuestSystem.AddQuest = function(plr, Value)
+	--This function is called when the player talks with a quest NPC (on the client)
+	--then it goes to the server that will require the the correct module(mission) that NPC gives
+	
+	local QuestName = Instance.new("StringValue") -- Creates a stringValue with the current quest name inside the "CurrentQuests" Folder
+	QuestName.Name = script.Name -- Holds the questName
+	QuestName.Parent = plr.CurrentQuests
 
-local AnimationCache = {}
+	local Goal = Instance.new("NumberValue") -- Creates a NumberValue with the goal the player needs to complete the quest, goes inside the "CurrentQuests" Folder
+	Goal.Name = "Goal"
+	Goal.Value = QuestSystem.Needed --how much the player needs to complete the quest
+	Goal.Parent = QuestName
 
-local RayParams = RaycastParams.new()
-RayParams.FilterType = Enum.RaycastFilterType.Include
+	local Progress = Instance.new("NumberValue") --  Creates a NumberValue with the players progress , goes inside the "CurrentQuests" Folder
+	Progress.Name = "Progress"
+	Progress.Value = Value or 0 -- Current progress
+	Progress.Parent = QuestName
 
-local function LoadAnimTrack(Hum, Animation, Data)
-	if not AnimationCache[Animation] then
-		AnimationCache[Animation] = Hum:LoadAnimation(Animation)
-		AnimationCache[Animation].Looped = Data.Looped or false
-		AnimationCache[Animation].Priority = Data.AnimationPriority or Enum.AnimationPriority.Action 
-	end
-	return AnimationCache[Animation]
+	QuestSystem.HandleQuest(plr, QuestName) -- After creating the quest, we call the handle for it
 end
 
-local System = {}
+QuestSystem.CancelQuest = function(plr)
+	--if the player wants to cancell the quest
+	local QuestsFolder = plr.CurrentQuests
 
-System.Active = function()
-	local Character = game.Players.LocalPlayer.Character
-	local RootPart = Character.HumanoidRootPart
-	local Hum = Character.Humanoid
-	
-	if Debounce == false then
-		Debounce = true
-		Holding = true
-		
-		local GroundFx = script.Vfx.GroundPiece:Clone()
-		GroundFx.Parent = RootPart
-		GroundFx.CFrame = RootPart.CFrame * CFrame.new(0,-3,0)
-		Utilities.Particle_Setup({Holder = GroundFx, Type = "Enable", Bool = true})
-		
-		local CeroSphereFx = script.Vfx.CeroSphereCharge:Clone()
-		CeroSphereFx.Parent = RootPart	
-		CeroSphereFx.CFrame = Character.Head.CFrame * CFrame.new(0, 0, -2)
-		Utilities.Particle_Setup({Holder = CeroSphereFx, Type = "Enable", Bool = true})
-		local tween = TW:Create(CeroSphereFx.Attachment.PointLight, TweenInfo.new(.35), {Brightness = 1.5})
-		tween:Play()
-		tween:Destroy()
-		
-		local Bp = Instance.new("BodyPosition")
-		Bp.Parent = RootPart
-		Bp.Name = "HoldCharBp"
-		Bp.MaxForce = Vector3.one * 4e4
-		Bp.D = 150
-		Bp.Position = RootPart.Position
-
-		local Bg = Instance.new("BodyGyro")
-		Bg.Parent = RootPart
-		Bg.Name = "HoldCharBg"
-		Bg.MaxTorque = Vector3.one * 4e4
-		Bg.CFrame = CFrame.lookAt(RootPart.Position, game.Players.LocalPlayer:GetMouse().Hit.Position)
-
-		spawn(function()
-			while Holding do 
-				CeroSphereFx.CFrame = Character.Head.CFrame * CFrame.new(0, 0, -2)
-				Bg.CFrame = CFrame.lookAt(RootPart.Position, game.Players.LocalPlayer:GetMouse().Hit.Position)
-				task.wait()
-			end
-			Utilities.Particle_Setup({Holder = CeroSphereFx, Type = "Enable", Bool = false})
-			Utilities.Particle_Setup({Holder = GroundFx, Type = "Enable", Bool = false})
-			local tween = TW:Create(CeroSphereFx.Attachment.PointLight, TweenInfo.new(.35), {Brightness = 0})
-			tween:Play()
-			tween:Destroy()
-			
-			delay(1.5, function()
-				CeroSphereFx:Destroy()
-				GroundFx:Destroy()
-			end)
-			Bp:Destroy()
-			Bg:Destroy()
-		end)
-	end
-	
+	QuestRmt:FireClient(plr, {Type = "UiControllerCancell"}) --Fire it to the client, so the QuestUI will be closed
+	QuestsFolder:ClearAllChildren() -- Remove the player current quest
 end
 
-System.End = function()
-	local Character = game.Players.LocalPlayer.Character
-	local RootPart = Character.HumanoidRootPart
-	local Hum = Character.Humanoid
-	
-	if Holding == true and Debounce == true then
-		Holding = false
-		delay(5, function()
-			Debounce = false
-		end)
-		
-		local InitPos = (Character.Head.CFrame * CFrame.new(0,0,-2)).Position
-		local EndPos = (RootPart.CFrame * CFrame.new(0,0,-1000)).Position
-		
-		local DustFx = script.Vfx.DustEmitter:Clone()
-		DustFx.Parent = RootPart
-		DustFx.CFrame = CFrame.lookAt((RootPart.CFrame * CFrame.new(0,-3,0)).Position, EndPos) * CFrame.Angles(0, math.rad(180),0) 
-		Utilities.Particle_Setup({Holder = DustFx, Type = "Enable", Bool = true})
-		
-		local CircleFx = script.Vfx.CircleFx:Clone()
-		CircleFx.Parent = RootPart
-		CircleFx.CFrame = Character.Head.CFrame * CFrame.new(0, 0, -2)
-		Utilities.Particle_Setup({Holder = CircleFx, Type = "Enable", Bool = true})
-		
-		delay(0.15,function()
-			local BeamFx = script.Vfx.BeamEmitter:Clone()
-			BeamFx.Parent = RootPart
-			BeamFx.CFrame = CFrame.lookAt(InitPos,EndPos) * CFrame.Angles(0,math.rad(180),0)
-			Utilities.Particle_Setup({Holder = BeamFx, Type = "Enable", Bool = true})
-			
-			local Impact = script.Vfx.Impact:Clone()
-			Impact.Parent = game.Lighting
-			task.delay(0.1,function()
-				local tween = TW:Create(Impact, TweenInfo.new(.1), {TintColor = Color3.fromRGB(255,255,255)})
-				tween:Play()
-				tween:Destroy()
-				task.delay(0.1,function()
-					Impact.Contrast = 2
-					task.delay(0.1,function()
-						Impact:Destroy()
-					end)
-				end)
-			end)
-			
-			local tween = TW:Create(BeamFx.ParticlePlaceHolder.PointLight, TweenInfo.new(.35), {Brightness = 2})
-			tween:Play()
-			tween:Destroy()
-			
-			spawn(function()
-				local Enablea = true
-				task.delay(3,function()
-					Enablea = false
-				end)
-				while Enablea do
-					CameraShake.ShakeOnce({3.5,3.5,0.2,0.6})
-					task.wait(0.06)
-				end
-			end)
-			
-			for _,v in pairs(BeamFx:GetChildren()) do
-				if v.Name == "A2" then
-					v.Position = Vector3.new(0,0,0)
-					local tween = TW:Create(v, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {Position = Vector3.new(0,0,70)})
-					tween:Play()
-					tween:Destroy()
-				end
-			end
-			
-			delay(3, function()
-				for _,v in pairs(BeamFx:GetChildren()) do
-					if v.Name == "A1" then
-						for i,a in pairs(v:GetChildren()) do
-							local tween = TW:Create(a, TweenInfo.new(0.3, Enum.EasingStyle.Sine, Enum.EasingDirection.Out), {CurveSize0 = 0, CurveSize1 = 0, Width0 = 0, Width1 = 0})
-							tween:Play()
-							tween:Destroy()
-						end
+QuestSystem.CompleteQuest = function(plr, QuestName)
+	--==To do:  Add Yen, Xp Rewards ==--
+	QuestRmt:FireClient(plr, {Type = "QuestCompleted"}) --Fire it to the client, so the QuestUI will be closed and the quest completed
+	print("QUEST COMPLETED!")
+	local QuestsFolder = plr.CurrentQuests
+	task.wait(3)
+	QuestsFolder:ClearAllChildren() -- Remove the player current quest
+end
+
+QuestSystem.HandleQuest = function(plr, QuestName) -- The quest funcion, how the quest works, what the player needs to do etc...
+	for _,Bananas in pairs(QuestItems:GetChildren()) do --Loops through all the bannas inside the Folder
+		if Bananas.Name == "Banana" then --Check if the value found is equal "bananas"
+			Bananas.ClickBanana.MouseClick:Connect(function(plr) -- if its a banana, we will start monitorating the mouseclick event that is inside of it
+				if plr.CurrentQuests:FindFirstChild(script.Name) then --cheks if the player have the quest, otherwise the script will not continue
+					QuestName.Progress.Value += 1 --adds +1 in te progress
+
+					if QuestName.Progress.Value == QuestSystem.Needed then -- checks if the progress is equal to the needed to complete the quest
+						QuestSystem.CompleteQuest(plr, QuestName) --calls the complete function
 					end
 				end
-				task.delay(.3, function()
-					Utilities.Particle_Setup({Holder = BeamFx, Type = "Enable", Bool = false})
-				end)
-				
-				local tween = TW:Create(BeamFx.ParticlePlaceHolder.PointLight, TweenInfo.new(.35), {Brightness = 0})
-				tween:Play()
-				tween:Destroy()
-				
-				
-				Utilities.Particle_Setup({Holder = CircleFx, Type = "Enable", Bool = false})
-				Utilities.Particle_Setup({Holder = DustFx, Type = "Enable", Bool = false})
 			end)
-		end)
-
-		
+		end
 	end
 end
 
-return System
+return QuestSystem
